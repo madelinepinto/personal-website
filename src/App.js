43fb65bf817c7b6c2d124ae879cd552dfb9d02e8
Madeline Pinto <madelinecpinto@gmail.com>
2024-01-11 21:49:52 -0500

    remove Navigate

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Loader from './components/Loader';
 
 
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
